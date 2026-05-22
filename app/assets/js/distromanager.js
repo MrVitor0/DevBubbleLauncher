617103b04d31df78c34aa37a3c0888636c2f03e4
Vitor Hugo <vto.hugo67@gmail.com>
2026-05-22 20:14:24 -0300

    chore: Use localhost distro URL and apply style fixes

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -1,17 +1,20 @@
-const { DistributionAPI } = require('helios-core/common')
+/* eslint-disable @stylistic/quotes */
+/* eslint-disable @stylistic/semi */
+const { DistributionAPI } = require("helios-core/common");
 
-const ConfigManager = require('./configmanager')
+const ConfigManager = require("./configmanager");
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
+exports.REMOTE_DISTRO_URL =
+    "http://localhost:3200/assets/distribution.json";   
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
     null, // Injected forcefully by the preloader.
     null, // Injected forcefully by the preloader.
     exports.REMOTE_DISTRO_URL,
-    false
-)
+    false,
+);
 
-exports.DistroAPI = api
+exports.DistroAPI = api;
